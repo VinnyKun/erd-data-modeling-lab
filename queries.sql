0d4f5593a4f5b345cf1999b3aea0c8e219f73dd8
vineethkunnathsg@gmail.com <vineethkunnathsg@gmail.com>
2018-06-28 23:56:50 +0800

    added queries

--- queries.sql
+++ queries.sql
@@ -1,9 +1,52 @@
+--To get every contributor of a partiuclar article
+
+-- output columns
+SELECT contributor_article.article_id, contributors.name
+
+-- from these two tables
+FROM contributors
+INNER JOIN contributor_article
+
+-- to find mathcing contributors
+ON (contributor_article.contributor_id = contributors.id)
+
+--Conditional: to set article =12
+WHERE contributor_article.article_id = 12;
+
+
+SELECT contributor_article.article_id, contributors.name
+FROM contributors
+INNER JOIN contributor_article
+ON (contributor_article.contributor_id = contributors.id)
+WHERE contributor_article.article_id = 12;
+
+-------------------------
+-------------------------
+
+--To get every article to which a contibutor contributed to
+
+-- output columns
+SELECT contributor_article.contributor_id, articles.name
+
+-- from these two tables
+FROM articles
+INNER JOIN contributor_article
+
+-- to find mathcing articles id
+ON (contributor_article.articles_id = articles.id)
+
+--Conditional: to set contributor =8
+WHERE contributor_article.contributor_id = 8;
 
 
 
-SELECT shop_products.shop_id, products.name
-FROM products
-INNER JOIN shop_products
-ON (shop_products.product_id = products.id)
-WHERE shop_products.shop_id = 2;
+SELECT contributor_article.contributor_id, articles.name
+FROM articles
+INNER JOIN contributor_article
+ON (contributor_article.article_id = articles.id)
+WHERE contributor_article.contributor_id = 8;
+
+
+-------------------------
+-------------------------
 
